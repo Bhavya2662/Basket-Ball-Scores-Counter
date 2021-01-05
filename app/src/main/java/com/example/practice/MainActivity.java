90720332ce8c2c0233c0add467c92245606b072d
Bhavya Sharma <bhavyaas2662@hotmail.com>
2021-01-05 17:29:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/practice/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.practice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    int scoreTeamA=0;
+    int scoreTeamB=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        displayForTeamA(scoreTeamA);
+    }
+    public void addThreeForTeamA(View v){
+        scoreTeamA +=3;
+        displayForTeamA(scoreTeamA);
+
+    }
+    public void addTwoForTeamA(View v){
+        scoreTeamA +=2;
+        displayForTeamA(scoreTeamA);
+
+    }
+    public void addOneForTeamA(View v){
+        scoreTeamA +=1;
+        displayForTeamA(scoreTeamA);
+
+    }public void addThreeForTeamB(View v){
+        scoreTeamB +=3;
+        displayForTeamB(scoreTeamB);
+
+    }
+    public void addTwoForTeamB(View v){
+        scoreTeamB +=2;
+        displayForTeamB(scoreTeamB);
+
+    }
+    public void addOneForTeamB(View v){
+        scoreTeamB +=1;
+        displayForTeamB(scoreTeamB);
+
+    }
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+    public void displayForTeamB(int score) {
+            TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+            scoreView.setText(String.valueOf(score));
+    }
+    public void reset(View v){
+        scoreTeamB=0;
+        scoreTeamA=0;
+        displayForTeamB(scoreTeamB);
+        displayForTeamA(scoreTeamA);
+    }
+
+}
